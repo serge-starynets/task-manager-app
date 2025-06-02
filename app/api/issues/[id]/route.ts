179af94e3583e158be343b002550ec9df34a769b
serge-starynets <sergii.starynets@gmail.com>
2025-06-02 13:47:46 +0800

    fix issues id route

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,29 +1,21 @@
-import { NextResponse } from 'next/server';
 import { db } from '@/db';
 import { issues } from '@/db/schema';
 import { eq } from 'drizzle-orm';
+import { NextRequest, NextResponse } from 'next/server';
 
-export async function GET(
-  request: Request,
-  { params }: { params: { id: any } },
-) {
+export const GET = async (
+  req: NextRequest,
+  { params }: { params: Promise<{ id: string }> },
+) => {
   try {
     const { id } = await params;
-
     const issue = await db.query.issues.findFirst({
-      where: eq(issues.id, id),
+      where: eq(issues.id, parseInt(id)),
     });
 
-    if (!issue) {
-      return NextResponse.json({ error: 'Issue not found' }, { status: 404 });
-    }
-
-    return NextResponse.json(issue);
-  } catch (error) {
-    console.error('Error fetching issues:', error);
-    return NextResponse.json(
-      { error: 'Failed to fetch issues' },
-      { status: 500 },
-    );
+    return NextResponse.json({ data: issue });
+  } catch (e) {
+    console.error(e);
+    return NextResponse.json({ error: 'not here' }, { status: 404 });
   }
-}
+};
